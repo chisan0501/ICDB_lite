7d55d90005c7cdb446d198c0318c821600996d40
SAM <SAM@SAM-PC>
2017-04-24 23:10:10 -0700

    repo1

---
+++ Demo/Demo/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        db_a094d4_demoEntities db = new db_a094d4_demoEntities();
+        public ActionResult Index()
+        {
+            var date_from = DateTime.Today.AddDays(-1);
+                var date_to =  DateTime.Today.AddDays(1);
+            
+            return View();
+        }
+
+        public JsonResult production_data ()
+        {
+            var discovery_data = (from d in db.discovery where d.time >= DateTime.Today.AddDays(-1) && d.time <= DateTime.Today.AddDays(1) select d).Count();
+            var rediscovery_data = (from d in db.rediscovery where d.time >= DateTime.Today.AddDays(-1) && d.time <= DateTime.Today.AddDays(1) select d).Count();
+            return Json(discovery_data,JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Demo/Demo/Controllers/production_logController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Demo;
+
+namespace Demo.Controllers
+{
+    public class production_logController : Controller
+    {
+        private db_a094d4_demoEntities db = new db_a094d4_demoEntities();
+
+        // GET: production_log
+        public ActionResult Index()
+        {
+            return View(db.production_log.ToList());
+        }
+
+        // GET: production_log/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            production_log production_log = db.production_log.Find(id);
+            if (production_log == null)
+            {
+                return HttpNotFound();
+            }
+            return View(production_log);
+        }
+
+        // GET: production_log/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: production_log/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "time,ictags,wcoa,ocoa,Manufacture,Model,CPU,RAM,HDD,serial,channel,pre_coa,location,video_card,screen_size")] production_log production_log)
+        {
+            if (ModelState.IsValid)
+            {
+                db.production_log.Add(production_log);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(production_log);
+        }
+
+        // GET: production_log/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            production_log production_log = db.production_log.Find(id);
+            if (production_log == null)
+            {
+                return HttpNotFound();
+            }
+            return View(production_log);
+        }
+
+        // POST: production_log/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "time,ictags,wcoa,ocoa,Manufacture,Model,CPU,RAM,HDD,serial,channel,pre_coa,location,video_card,screen_size")] production_log production_log)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(production_log).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(production_log);
+        }
+
+        // GET: production_log/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            production_log production_log = db.production_log.Find(id);
+            if (production_log == null)
+            {
+                return HttpNotFound();
+            }
+            return View(production_log);
+        }
+
+        // POST: production_log/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            production_log production_log = db.production_log.Find(id);
+            db.production_log.Remove(production_log);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
